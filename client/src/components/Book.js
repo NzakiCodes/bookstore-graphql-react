8ad886fd565122ba10650bd95833c5460cc3d957
NzakiCodes <ekisnzaki@gmail.com>
2020-08-10 20:26:39 +0100

    Update Book.js

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -8,17 +8,36 @@ const Book = ({ id }) => {
     });
 
     if (loading) return <p>Loading...</p>;
-    if (error) return <p style={{ color: "red" }}>Error: {error.message}</p>
-    return (
-        <div>
-            {
-                <div>
-                    Book Title: {data.book.name}<br />
-                        Authored by {data.book.author.name}
-                </div>
-            }
-        </div>
-    )
+    // if (error) return <p style={{ color: "red" }}>Error: {error.message}</p>
+    if (data) {
+        return (
+            <div>
+                <h1>Book Details</h1>
+                {
+                    <div>
+
+                        <h2>Book Title: {data.book.name}</h2>
+                        <h4>Genre: {data.book.genre}</h4>
+                        <h4>Author: {data.book.author.name}</h4>
+                        <h4>Age: {data.book.author.age}</h4>
+                        <h4> Books by {data.book.author.name}:</h4>
+                        <ul>
+                            {
+                                data.book.author.books.map(({ id, name }) => (
+                                    <li key={id}>{name}</li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                }
+
+            </div>
+        )
+    } else {
+        return (
+            <h4>No Book Selected...</h4>
+        )
+    }
 }
 
 export default Book
