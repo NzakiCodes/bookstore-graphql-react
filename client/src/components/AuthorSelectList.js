80bb2fb56aa07e2c997ae84ed91060f2c58a325d
NzakiCodes <ekisnzaki@gmail.com>
2020-08-10 20:26:44 +0100

    Update AuthorSelectList.js

--- client/src/components/AuthorSelectList.js
+++ client/src/components/AuthorSelectList.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { useQuery } from '@apollo/client'
 import { GET_AUTHORS } from '../queries/queries'
-
 const AuthorSelectList = () => {
     const { loading, error, data } = useQuery(GET_AUTHORS);
-
+    console.log(error)
     if (loading) {
         return <option>Loading...</option>
     } else if (error) {
